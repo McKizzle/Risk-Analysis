04038a850a7ebd26e91f0b91c99de6a8f44b7500
Clinton McKay <ziesunkownemail@clueless.duh>
2013-12-01 23:18:41 -0700

    sunday night commit

--- src/main.R
+++ src/main.R
@@ -15,8 +15,10 @@ source("./utils/dump2arr.R")
 
 llibrary('ISOcodes')
 llibrary('ggplot2') #contains the map_data function.
-llibrary('maps')
+#llibrary('maps')
 llibrary('maptools')
+llibrary('rgeos')
+llibrary('mapdata')
 # llibrary('spatstat')
 
 # get the countries in the UN_M ISO and extract teh set of countries that match
@@ -36,9 +38,14 @@ not.in.un.countries <- unique(map.data$region[
   ])
 
 # Lets aquire the countries from the shapefiles I downloaded from the internet.
-countries.shp = readShapeSpatial(
-  fn='../data/mapdata/TM_WORLD_BORDERS-0.3//TM_WORLD_BORDERS-0.3.shp', 
+countries.shp <- readShapeSpatial(
+  fn='../data/mapdata/TM_WORLD_BORDERS-0.3/TM_WORLD_BORDERS-0.3.shp', 
   proj4string=CRS("+proj=longlat +ellps=clrk66"),
   verbose=T
 )
+
+un.mappings = cbind('iso3.numeric'=countries.shp$UN, 'region'=countries.shp$REGION, 'subregion'=countries.shp$SUBREGION)
+write.csv(un.mappings, file = 'UNCountriesToRegions.csv', row.names=F)
+
+map('world2Hires', xlim=c(0, 360))
 
